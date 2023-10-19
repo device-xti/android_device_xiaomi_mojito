e63c2aec8499910106e09d5efc4ef1663046eacd
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    mojito: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -406,7 +406,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QMI
 PRODUCT_PACKAGES += \
