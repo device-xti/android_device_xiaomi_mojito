e257c7cb52bb26e3f9882ba825d6dd498ceca440
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2025-02-27 11:02:58 +0700

    mojito: remove some duplicate PRODUCT packages

--- device.mk
+++ device.mk
@@ -337,9 +337,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-PRODUCT_PACKAGES += \
-    libpiex_shim
-
 # Overlay
 PRODUCT_PACKAGES += \
     MojitoAperture \
